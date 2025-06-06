5eab4a61910d3a321cace5bcf666be7042739ded
prashant singh <prashantsingh@prashants-MacBook-Air.local>
2025-06-07 01:37:32 +0530

    init project

---
+++ src/main/java/com/spring/basics/spring_movie_recommender_system/SpringMovieRecommenderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.spring.basics.spring_movie_recommender_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMovieRecommenderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMovieRecommenderSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/basics/spring_movie_recommender_system/SpringMovieRecommenderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.basics.spring_movie_recommender_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMovieRecommenderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
